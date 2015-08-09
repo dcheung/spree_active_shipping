268dd7873c561f9a3821c1de65171c30b745161f
Dan Cheung <dcheung@users.noreply.github.com>
2015-08-09 00:59:31 -0700

    Update base.rb

--- app/models/spree/calculator/shipping/fedex/base.rb
+++ app/models/spree/calculator/shipping/fedex/base.rb
@@ -1,5 +1,4 @@
 require_dependency 'spree/calculator'
-require 'active_shipping'
 
 module Spree
   module Calculator::Shipping
@@ -14,7 +13,7 @@ module Spree
             :test => Spree::ActiveShipping::Config[:test_mode]
           }
 
-          ActiveShipping::FedEx.new(carrier_details)
+          ::ActiveShipping::FedEx.new(carrier_details)
         end
       end
     end
